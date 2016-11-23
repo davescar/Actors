14ea66617be3e69f83e42343198bcb684a1f50a5
dave-scar <david.scarimbolo@gmail.comgit config --global user.email david.scarimbolo@gmail.com>
2016-11-23 10:56:59 -0500

    actors

--- Actors/app/src/main/java/drsdrs/actors/MainActivity.java
+++ Actors/app/src/main/java/drsdrs/actors/MainActivity.java
@@ -2,12 +2,36 @@ package drsdrs.actors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+
+
+        LinearLayoutManager linearLayoutManager =
+                new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+
+        ArrayList<Actor> actors = new ArrayList<>();
+
+        actors.add(new Actor("Joe", "November", "12"));
+        actors.add(new Actor("Jim", "July", "12"));
+        actors.add(new Actor("Jack", "March", "12"));
+
+
+
+
     }
+}
 }
