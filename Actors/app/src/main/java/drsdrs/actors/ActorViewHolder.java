781996f1a9d1ca1073d5a47ab3de41c95e9215cd
dave-scar <david.scarimbolo@gmail.comgit config --global user.email david.scarimbolo@gmail.com>
2016-11-23 09:52:56 -0500

    added viewholder and xml

---
+++ Actors/app/src/main/java/drsdrs/actors/ActorViewHolder.java
@@ -0,0 +1,25 @@
+package drsdrs.actors;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by ds on 11/23/16.
+ */
+
+public class ActorViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView mName;
+    private TextView mDOB;
+    private TextView mOscars;
+
+    public ActorViewHolder(View itemView) {
+        super(itemView);
+
+        mName = (TextView) itemView.findViewById(R.id.)
+
+    }
+
+
+}
